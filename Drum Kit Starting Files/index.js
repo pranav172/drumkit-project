c2cee10820b9ae9b7c255e00557594adae63ba2d
pranav172 <93277763+pranav172@users.noreply.github.com>
2023-01-22 15:14:11 +0530

    Add files via upload

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,59 @@
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+    soundOf(this.innerHTML);
+    setAnimation(this.innerHTML);
+  });
+
+}
+document.addEventListener("keydown", function(event) {
+  soundOf(event.key);
+  setAnimation(event.key);
+})
+
+function soundOf(key) {
+
+  switch (key) {
+    case "w":
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
+    case "a":
+      var kick = new Audio('sounds/kick-bass.mp3');
+      kick.play();
+      break;
+    case "s":
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+    case "d":
+      var tom1 = new Audio('sounds/tom-1.mp3');
+      tom1.play();
+      break;
+    case "j":
+      var tom2 = new Audio('sounds/tom-2.mp3');
+      tom2.play();
+      break;
+    case "k":
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+    case "l":
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+    default:
+      console.log("press the visible key");
+
+  }
+
+
+}
+
+function setAnimation(action) {
+var active = document.querySelector("." + action);
+  active.classList.add("pressed");
+  setTimeout(function(){active.classList.remove("pressed")
+} , 100);
+}
